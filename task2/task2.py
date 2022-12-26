689b8b318ac96ff7ed37f601be9daa9d7a41d63d
Sten09 <sten111982@gmail.com9>
2022-12-26 12:45:44 +0300

    first commit

---
+++ task2/task2.py
@@ -0,0 +1,9 @@
+#Напишите программу для проверки ложности утверждения
+
+#(W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) для всех значений предикат.
+
+for x in range (2):
+for y in range (2):
+for z in range (2):
+    
+print((x,y,z), not (x or y or z) == (not x and not y and not z))
